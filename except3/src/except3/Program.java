10cdaff4478378eebb229f0f8dec4381d775e326
shashank <shashank944533@gmail.com>
2023-02-23 10:13:49 +0530

    Pattern print

---
+++ except3/src/except3/Program.java
@@ -0,0 +1,19 @@
+package except3;
+
+public class Program {
+public static void main(String []args)
+{
+	for(int i= 0;i<=5;i++)
+	{
+		if(i == 3)
+		{
+			continue;
+		}
+		else
+		{
+			System.out.println(i);
+		}
+	}
+	
+}
+}
