bd99539ccad06cf9481dc2fccd0d0a572cdf07da
shashank <shashank944533@gmail.com>
2023-02-23 10:16:58 +0530

    Pattern Print $*

---
+++ dollarStarPatternPrint/src/newjava/Program.java
@@ -0,0 +1,33 @@
+package newjava;
+
+public class Program {
+public static void main(String []args) 
+{
+	for(int i=1;i<=5;i++)
+	{
+		System.out.print("*");
+	}
+	System.out.println();
+	for(int i=1;i<=5;i++)
+	{
+		System.out.print("$");
+	}
+	System.out.println();
+
+	for(int i=1;i<=5;i++)
+	{
+		System.out.print("*");
+	}
+	System.out.println();
+	for(int i=1;i<=5;i++)
+	{
+		System.out.print("$");
+	}
+	System.out.println();
+
+	for(int i=1;i<=5;i++)
+	{
+		System.out.print("*");
+	}
+}
+}
