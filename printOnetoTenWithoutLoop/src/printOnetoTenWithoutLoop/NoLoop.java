a3a7bfae439e9a394b813891a4ee9c92eefa6fd8
shashank <shashank944533@gmail.com>
2023-02-28 20:48:30 +0530

    Print 1-10 with no loop

---
+++ printOnetoTenWithoutLoop/src/printOnetoTenWithoutLoop/NoLoop.java
@@ -0,0 +1,15 @@
+package printOnetoTenWithoutLoop;
+
+public class NoLoop {
+public static void main(String[] args) {
+	printNumber(1);
+}
+public static void printNumber(int n)
+{
+	if(n <= 10)
+	{
+		System.out.println(n);
+		printNumber(n+1);
+	}
+}
+}
