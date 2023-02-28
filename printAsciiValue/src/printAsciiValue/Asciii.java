ff6af706f4828ecbea5141dc10ef290306f87fc7
shashank <shashank944533@gmail.com>
2023-02-28 20:46:56 +0530

    Printing ascii value of character

---
+++ printAsciiValue/src/printAsciiValue/Asciii.java
@@ -0,0 +1,12 @@
+package printAsciiValue;
+
+public class Asciii {
+public static void main(String[] args) {
+	char ch = 'z';
+	int i = ch;
+	int x = ch;
+	float z = ch;
+	System.out.println("Ascii value of "+ch+"="+i);
+	System.out.println("Ascii value of "+ch+"="+z);
+}
+}
