2df33b59c734ed89c336d02391766f2d23bb025a
shashank <shashank944533@gmail.com>
2023-02-28 20:41:00 +0530

    Panagram check

---
+++ panagramCheck/src/panagramCheck/Panagram.java
@@ -0,0 +1,24 @@
+package panagramCheck;
+
+public class Panagram {
+public static void main(String[] args) {
+	String s = "abcdefghijklmnopqrstuvwxyz";
+	System.out.println(isPanagram(s.toLowerCase()));
+}
+private static boolean isPanagram(String s)
+{
+	if(s.length() < 26)
+	{
+		return false;
+	}else {
+		for(char ch='a';ch<='z';ch++)
+		{
+			if(s.indexOf(ch)<0)
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+}
