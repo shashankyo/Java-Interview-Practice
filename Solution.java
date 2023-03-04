69c4832f9e60d40a406be100886eaa99e371d60c
shashankyo <114981861+shashankyo@users.noreply.github.com>
2023-03-04 08:59:10 +0530

    min and max range for various datatypes

---
+++ Solution.java
@@ -0,0 +1,28 @@
+package minMaxTry;
+
+public class Solution {
+public static void main(String[] args) {
+	System.out.println(Character.MAX_VALUE);
+	System.out.println(Character.MIN_VALUE);
+	
+	System.out.println("****************************");
+	
+	System.out.println(Integer.MAX_VALUE);
+	System.out.println(Integer.MIN_VALUE);
+	System.out.println("****************************");
+
+	System.out.println(Byte.MAX_VALUE);
+	System.out.println(Byte.MIN_VALUE);
+	
+	System.out.println("****************************");
+	
+	System.out.println(Double.MAX_VALUE);
+	System.out.println(Double.MIN_VALUE);
+	System.out.println("****************************");
+	
+	System.out.println(Short.MAX_VALUE);
+	System.out.println(Short.MIN_VALUE);
+
+	}
+}
+
